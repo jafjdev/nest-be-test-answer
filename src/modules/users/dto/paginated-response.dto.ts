1d19028d7770d7022946b31929023176ccadfbe2
jose Cedeno <jafjdev@gmail.com>
2023-06-07 15:54:31 +0200

    fix: Using parseInt in limit,page and sort.

--- src/modules/users/dto/paginated-response.dto.ts
+++ src/modules/users/dto/paginated-response.dto.ts
@@ -21,5 +21,8 @@ export class PaginatedResponseDto<T> implements IPaginatedResponse<T> {
 
   constructor(args?: PaginatedResponseDto<T>) {
     Object.assign(this, args);
+    this.limit = parseInt(this.limit as any);
+    this.page = parseInt(this.page as any);
+    this.sort = parseInt(this.sort as any);
   }
 }
