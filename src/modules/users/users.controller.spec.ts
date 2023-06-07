c15e58b1392579bf22cab81d29f82b42728a77be
jose Cedeno <jafjdev@gmail.com>
2023-06-07 15:52:15 +0200

    fix: using expect from @jest/globals in order to solve cypress integration.

--- src/modules/users/users.controller.spec.ts
+++ src/modules/users/users.controller.spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
-
+import { expect } from '@jest/globals';
 describe('UsersController', () => {
   let controller: UsersController;
 
