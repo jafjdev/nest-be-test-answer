98dc9dcec41aaa8a221b23a484841e8987bc51ec
jose Cedeno <jafjdev@gmail.com>
2023-06-09 01:06:44 +0200

    fix: Solving imports in .spect files.

--- src/modules/users/interceptors/users.interceptor.spec.ts
+++ src/modules/users/interceptors/users.interceptor.spec.ts
@@ -1,5 +1,5 @@
 import { UsersInterceptor } from './users.interceptor';
-
+import { expect } from '@jest/globals';
 describe('UsersInterceptor', () => {
   it('should be defined', () => {
     expect(new UsersInterceptor()).toBeDefined();
--- src/modules/users/users.service.spec.ts
+++ src/modules/users/users.service.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { expect } from '@jest/globals';
 
 describe('UsersService', () => {
   let service: UsersService;
